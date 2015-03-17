02f3de954daace38a09d282cd8dedd74bbfa9c68
Michael Adaixo <michael.adaixo@gmail.com>
2015-03-17 21:08:01 +0000

    Adds basic structure with login/logout. Support for avatar upload on devise with paperclip

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,48 @@
 class DashboardController < ApplicationController
+	before_action :authenticate_user!
+
+	def index
+		@user = current_user
+	end
+
+	def show
+		
+	end
+
+	def new
+		
+	end
+
+	def create
+		
+	end
+
+	def edit
+	end
+
+	def update
+		
+	end
+
+	def destroy
+		
+	end
+
+	def upvote
+		
+	end
+
+	def downvote
+		
+	end
+
+	private
+
+	def find_post
+		
+	end
+
+	def post_params
+		
+	end
 end
